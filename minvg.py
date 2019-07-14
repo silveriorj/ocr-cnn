a32514abb1818f02cc828c8b92cc09ab3e7797b0
Raul Silvério <rjosesilverio@gmail.com>
2019-07-14 03:58:45 -0300

    OCR with 5 characters recognition

---
+++ minvg.py
@@ -0,0 +1,47 @@
+from keras import Sequential
+from keras import backend as K
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, BatchNormalization, Dropout
+
+
+class minvg:
+
+    @staticmethod
+    def build(width, height, depth, classes):
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+        # CONV => RELU => POOL
+        model.add(Conv2D(64, (3, 3), padding="same", input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU)*2 => POOL
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # FC => RELU
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # SOFTMAX CLASSIFIER
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        return model
+
